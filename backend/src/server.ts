6c2b1970f54979aecc1b4775be0168efbf750cd2
Robbottone <gallu.roberto@gmail.com>
2025-06-17 23:49:32 +0200

    feat - prima get per ping pong

---
+++ backend/src/server.ts
@@ -0,0 +1,22 @@
+//devo importare express - cors - dotenv.
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+//devo inizializzare express e cors.
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001
+app.use(cors());
+app.use(express.json())
+
+app.get('/ping', (req, res) => {
+    res.status(200).send('pong');
+})
+
+//da qui in poi devo importare le rotte.
+
+app.listen(PORT, () => {
+  console.log(`✅ Backend in ascolto su http://localhost:${PORT}`)
+})
