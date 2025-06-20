18c96683e643dccc8548b8180a7a7f4f368a24af
Robbottone <gallu.roberto@gmail.com>
2025-06-20 17:05:29 +0200

    feat - create e delete menu in router

--- backend/src/routes/menu/menu.ts
+++ backend/src/routes/menu/menu.ts
@@ -6,7 +6,9 @@ const menuRoute = Router()
 
 menuRoute.get('/menu', tenantLoader, menuController.getMenu);
 menuRoute.get('/menu/:id', tenantLoader, menuController.getMenuById);
+menuRoute.post('/menu', tenantLoader, menuController.createMenu);
 menuRoute.post('/menu/:menuid/items', tenantLoader, menuController.addItemsToMenu);
 menuRoute.delete('/menu/:menuId/items', tenantLoader, menuController.removeItemsFromMenu);
+menuRoute.delete('/menu/:menuId', tenantLoader, menuController.deleteMenu);
 
 export default menuRoute
